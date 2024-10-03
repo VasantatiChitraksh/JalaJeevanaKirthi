704e411f246559b9c5b6cb4cff56994a8813cab8
Chitraksh Vasantati <149754440+VasantatiChitraksh@users.noreply.github.com>
2024-10-03 22:33:59 +0530

    update home

--- WebD/client/src/Homepage/home.jsx
+++ WebD/client/src/Homepage/home.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import './style.css';
 
 function Home() {
   return (
-    <div>
-      <h2>Welcome to My Custom Component!</h2>
-      <p>This is a custom JSX component rendered instead of the default content.</p>
-    </div>
+    <body>
+        <div className="about-section">
+            <h2>About Us</h2>
+            <p>We have started to to do this to save the marine life and stuff</p>
+        </div>
+        <div className="icons-section">
+            <div className="icon-box">
+                <p>Icon 1</p>
+            </div>
+            <div className="icon-box">
+                <p>Icon 2</p>
+            </div>
+            <div className="icon-box">
+                <p>Icon 3</p>
+            </div>
+        </div>
+        <footer className="footer-section">
+            <p>@ 2024 Your Company.All rights reseved</p>
+        </footer>
+    </body>
   );
 }
 
