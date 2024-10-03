5557ffd9c8b01a2c072aab0de1ea98674c4e90dc
Chitraksh Vasantati <149754440+VasantatiChitraksh@users.noreply.github.com>
2024-10-03 21:25:51 +0530

    Update

--- WebD/client/src/Homepage/home.jsx
+++ WebD/client/src/Homepage/home.jsx
@@ -1,2 +1,12 @@
-import React from "react";
-import ReactDom from "react-dom";
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to My Custom Component!</h2>
+      <p>This is a custom JSX component rendered instead of the default content.</p>
+    </div>
+  );
+}
+
+export default Home;
