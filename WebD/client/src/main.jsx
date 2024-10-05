42c1a53fd9947a725576c38c0c57058a0497bb21
CharanTej-3457 <cs23b011@iittp.ac.in>
2024-10-05 12:50:27 +0530

    yo

--- WebD/client/src/main.jsx
+++ WebD/client/src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Login from './Login.jsx'
+import Signin from './Sign in.jsx'
+import { FaSignInAlt } from 'react-icons/fa'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
+    <Login />
   </StrictMode>,
 )
