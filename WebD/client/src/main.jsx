32d64e98b6688d7f81809c87dfbe7333e61eea72
Chitraksh Vasantati <149754440+VasantatiChitraksh@users.noreply.github.com>
2024-10-06 19:26:14 +0530

    Update Revert

--- WebD/client/src/main.jsx
+++ WebD/client/src/main.jsx
@@ -1,16 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './components/App/App.jsx'
+import App from './App.jsx'
 import './index.css'
-import Login from './components/Login/Login.jsx'
-import Signin from './components/Signin/Signin.jsx'
-import Chat from './components/chat/chat.jsx'
-import { FaSignInAlt } from 'react-icons/fa'
-import StoryGen from './ML/story_gen.jsx'
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+<<<<<<< HEAD
     <App/>
+=======
+    <App />
+>>>>>>> parent of 40aa08b (Merge branch 'main' into chitraksh)
   </StrictMode>,
 )
