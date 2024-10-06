0d54add5a45208b8459ac1c2bf56cdd938494017
Chitraksh Vasantati <149754440+VasantatiChitraksh@users.noreply.github.com>
2024-10-06 19:26:57 +0530

    Revert "Delete App.jsx"
    This reverts commit e360a92b547d9ebb6dcf28d345868450a7c54eeb.

---
+++ WebD/client/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+
+import React from 'react';
+import Home from './Homepage/home.jsx';  // Import Home component
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App; // Export the App component
