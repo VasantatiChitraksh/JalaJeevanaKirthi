ed9f8a45a21672ac3aa6691da5454020b3fed4de
BhanuprakashCH <cs23b012@iittp.ac.in>
2024-10-07 19:37:56 +0530

    changes in main

--- WebD/client/src/main.jsx
+++ WebD/client/src/main.jsx
@@ -1,16 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import Home from '../src/Homepage/home.jsx';
-import './index.css'
-import Login from './components/Login/Login.jsx'
-import Signin from './components/Signin/Signin.jsx'
-import Chat from './components/chat/chat.jsx'
-import { FaSignInAlt } from 'react-icons/fa'
-import StoryGen from './ML/story_gen.jsx'
-
+import './index.css';
+import Login from './components/Login/Login.jsx';
+import Signin from './components/Signin/Signin.jsx';
+import Chat from './components/chat/chat.jsx';
+import { FaSignInAlt } from 'react-icons/fa';
+import StoryGen from './ML/story_gen.jsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'; // Import Routes instead of Router
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Chat/>
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes> {/* Use Routes for defining multiple Route paths */}
+        <Route path='/signup' element={<Signin />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/home' element={ <> <Home />  <Chat/></>} />
+      </Routes>
+    </BrowserRouter>
+    
+  </StrictMode>
+);
