5f9b0d47bcd2939a0908b7caeabacec7a98e3955
Chitraksh Vasantati <149754440+VasantatiChitraksh@users.noreply.github.com>
2024-10-28 21:51:21 +0530

    Updated Game CSS

--- WebD/client/src/Game/game.jsx
+++ WebD/client/src/Game/game.jsx
@@ -1,34 +1,32 @@
 import React, { useEffect } from 'react';
 import Phaser from 'phaser';
+import './game.css';
+import { Link,Navigate,useNavigate } from "react-router-dom";
+import logo from '../assets/LOGO.jpg';
 import Coral from '../assets/Coral.png';
 import whale from '../assets/whale.png';
 
 function FishCatch() {
     useEffect(() => {
+        const scale = window.devicePixelRatio;
+        const w = window.innerWidth * scale;
+        const h = window.innerHeight * scale;
         let gamescene = new Phaser.Scene('Game');
         gamescene.preload = function () {
             this.load.image('background',Coral);
             this.load.image('player',whale);
         };
         gamescene.create = function () {
-            let bg = this.add.sprite(0,0,'background');
-            let gameW = this.sys.game.config.width;
-            let gameH = this.sys.game.config.height;
-            bg.setPosition(gameW/2,gameH/2);
-            bg.setScale(1.25,1.25);
-
-            let pl = this.add.sprite(150,100,'player');
-            pl.angle++;
-            pl.ScaleX = 1;
-            pl.scaleY = 1;
+            this.pl = this.add.sprite(100,500,'player');
         };
         gamescene.update = function () {
+            this.pl.x +=1;
         };
 
         let config = {
             type: Phaser.AUTO,
-            height: 360,
-            width: 640,
+            height: h-100,
+            width: w,
             scene: gamescene,
             parent: 'phaser-game' 
         };
@@ -38,10 +36,22 @@ function FishCatch() {
             game.destroy(true);
         };
     }, []);
-
+    const navigate = useNavigate();
+    const handleOnclick1 = (e) => {
+        navigate('/login');
+     }
+    const handleOnclick2 = (e) => {
+        navigate('/');
+     }
     return (
         <div className='main'>
             <div id="phaser-game"></div>
+            <div className="topbar">
+                <img className="logo-icon" src={logo} alt="Logo" />
+                <h2 className="logo">Jala Jeevana Kirthi</h2>
+                <button className="topbar-button" id="home_button" onClick={handleOnclick2}>Home</button>
+                <button className="topbar-button" id="login_button" onClick={handleOnclick1}>Login</button>
+            </div>
         </div>
     );
 }
