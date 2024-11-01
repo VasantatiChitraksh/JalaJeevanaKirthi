62a4fdcd46b07ad29db5a9719d1de3995b66bcf0
CharanTej-3457 <cs23b011@iittp.ac.in>
2024-11-01 13:22:39 +0530

    r

---
+++ WebD/client/src/Blog/blogHome.jsx
@@ -0,0 +1,16 @@
+import {Grid} from '@mui/material';
+
+import Banner from './banner'
+
+
+const blogHome = () =>{
+
+    return (
+        <> 
+         <Banner/> 
+        
+        </>
+    )
+}
+
+export default blogHome;
