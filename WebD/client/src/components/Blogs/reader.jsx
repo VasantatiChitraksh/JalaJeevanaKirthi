e70ec0de1e5771c8e42224ed816b92accbe9dcbe
CharanTej-3457 <cs23b011@iittp.ac.in>
2024-11-10 22:59:27 +0530

    yo

--- WebD/client/src/components/Blogs/reader.jsx
+++ WebD/client/src/components/Blogs/reader.jsx
@@ -1,1 +1,30 @@
+import React from 'react';
+import './Modal.css';
+
+function Modal({ post, onClose, onAuthorChange }) {
+  if (!post) return null; // Return null if there's no post to display
+  
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <button className="close-btn" onClick={onClose}>X</button>
+        <h1>{post.title}</h1>
+        <p><strong>Author:</strong> 
+          <input 
+            type="text" 
+            value={post.author} 
+            onChange={onAuthorChange} 
+            className="author-input" 
+          />
+        </p>
+        <p><strong>Date:</strong> {post.date}</p>
+        <div className="post-details">
+          <p>{post.content}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
 
