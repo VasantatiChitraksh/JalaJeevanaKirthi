f52368c3debdbf7a6e72d4b57d315ae4060a6dc4
AnirudhArrepu <anirudharrepu@gmail.com>
2024-11-17 15:59:54 +0530

    connected frontend to backend

--- WebD/server/index.js
+++ WebD/server/index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://jalajeevanakeerthi.vercel.app/",
     credentials: true,
     methods: ['GET', 'POST', 'OPTIONS'], 
     allowedHeaders: ['Content-Type', 'Authorization'],
@@ -42,7 +42,7 @@ const server = createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://jalajeevanakeerthi.vercel.app/",
         methods: ["GET", "POST"],
         credentials: true
     },
