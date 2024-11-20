40f3df8c9b5e9d714f9925891b2cf244d7d05850
AnirudhArrepu <anirudharrepu@gmail.com>
2024-11-20 16:48:53 +0530

    bug fixes

--- WebD/server/routes/blogs.js
+++ WebD/server/routes/blogs.js
@@ -15,28 +15,39 @@ router.get("/allblogs", async (req, res) => {
 });
 
 
-router.post("/newblog", async (req, res)=>{
-    const { username, title, content } = req.body;
+router.post("/newblog", async (req, res) => {
+    try {
+        const { username, title, content } = req.body;
 
-    const newBlog = new Blogs({
-        user: username,
-        title,
-        content,
-    })
+        const newBlog = new Blogs({
+            user: username,
+            title,
+            content,
+        });
 
-    await newBlog.save();
-    const flaskResponse = await axios.post("http://localhost:5000/addData", {
-        newdata: content,
-    });
-    
-    if (flaskResponse.data) {
-        return res.json({ status: "true", message: "Blog added and sent to knowledge base" });
-    } else {
-        return res.status(500).json({ status: "false", message: "Blog added but failed to update knowledge base" });
+        await newBlog.save();
+
+        try {
+            const flaskResponse = await axios.post("http://localhost:5000/addData", {
+                newdata: content,
+            });
+
+            if (flaskResponse.data) {
+                return res.json({ status: "true", message: "Blog added and sent to knowledge base" });
+            } else {
+                return res.status(500).json({ status: "false", message: "Blog added but failed to update knowledge base" });
+            }
+        } catch (error) {
+            console.error("Error communicating with Flask service:", error.message);
+            return res.status(500).json({
+                status: "false",
+                message: "Blog added but failed to communicate with the knowledge base.",
+            });
+        }
+    } catch (error) {
+        console.error("Error in /newblog route:", error.message);
+        return res.status(500).json({ status: "false", message: "An error occurred while adding the blog." });
     }
-
-    // return res.json({ status: "true", message: "Blog added" });
-    
-})
+});
 
 export {router as BlogsRouter}
